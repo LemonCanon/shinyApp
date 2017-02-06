167e50c7ffdf5e69b70c12814345169dc88f9aae
burrbank <ryan_j_porter@hotmail.ca>
2017-02-06 15:15:43 -0400

    add app files

---
+++ server.R
@@ -0,0 +1,37 @@
+library(shiny)
+library(leaflet)
+
+
+earth <- read.csv("database.csv")
+tmp <- as.Date(earth$Date, "%m/%d/%Y") %>% format("%Y") %>% as.numeric()
+earth$year <- tmp
+rm(tmp)
+earth <- earth[!is.na(earth$year) & !is.na(earth$Longitude) & !is.na(earth$Latitude),]
+earth <- earth[earth$year >= 2000,]
+
+shinyServer(function(input, output) {
+    
+   dataset <- eventReactive(input$button, {
+        in_years_low <- input$year_select[1] <= earth$year
+        in_years_high <- input$year_select[2] >= earth$year
+        in_mag_low <- input$mag_select[1] <= earth$Magnitude
+        in_mag_high <- input$mag_select[2] >= earth$Magnitude
+        earth[in_years_low & in_years_high &
+                      in_mag_low & in_mag_high,]
+    })
+    
+    
+    output$leaf <- renderLeaflet({
+        l <- leaflet(dataset()) %>% addTiles() %>% 
+            addCircleMarkers(stroke = FALSE, fillOpacity = 0.33, radius = 6,
+                             popup =~ paste(sep = "</br>", Date, Type, 
+                                            paste("magnitude:", Magnitude),
+                                            paste("Latitude:", Latitude),
+                                            paste("Longitude:", Longitude)))
+            
+            
+        print(l)
+    
+    })
+    shiny:::flushReact()
+})
---
+++ ui.R
@@ -0,0 +1,40 @@
+library(shiny)
+library(leaflet)
+
+
+shinyUI(fluidPage(
+    fluidRow(
+        column(12,
+               h1("Significant Earthquakes, 2000-2016 Map")
+               )
+    ),
+    fluidRow(
+        column(12,
+               p("by Ryan J. Porter")
+               )
+    ),    
+    fluidRow(
+        column(8,
+               leafletOutput("leaf")),
+        column(4,
+               p(strong("To start click the 'submit' button."),
+                 br("Each blue circle represents an earth quake. Use the sliders to select the years and magnitudes 
+                    shown, click submit to apply changes to the sliders. "),
+                 br("Click a circle to display the exact date and magnitude of that quake."),
+                 br("The dataset used is from the",
+                    HTML("<a href=https://www.kaggle.com/usgs/earthquake-database>earthquake database</a>"),
+                    "on ", HTML("<a href=https://www.kaggle.com/>kaggle.com</a>."))
+                 ))
+    ),
+    fluidRow(
+        column(4,
+               sliderInput("year_select", "Select Year Range", min = 2000, max = 2016, value = c(2000, 2016), sep = "")),
+        column(4,
+               sliderInput("mag_select", "Select Magnitude Range", min = 0, max = 10, value = c(0, 10))
+               ),
+        column(2,
+               actionButton("button", "submit")
+        )
+    )
+))
+
